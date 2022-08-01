4d1f03a4f9273f8f99df316ce555afc6ee9a868b
JCicero-Code <109885635+JCicero-Code@users.noreply.github.com>
2022-08-01 11:24:19 -0300

    Manipula string
    Exercício de manipulação de strings

---
+++ exercManipularString.js
@@ -0,0 +1,25 @@
+﻿
+function converter(texto){
+    numeroConvertido = ""
+    numero = parseInt(texto)
+    numeroConvertido = numero + 2
+    return numeroConvertido
+    
+}
+console.log(converter("10"));
+
+
+
+function mudarMaiuscula(nome){
+    nomeUpper = nome.toUpperCase()
+    return nomeUpper
+    
+}
+console.log(mudarMaiuscula("peter park"));
+
+
+function Contar(palavra){
+    numeroCaractere = palavra.length
+    return numeroCaractere
+}
+console.log(Contar("softex"))
